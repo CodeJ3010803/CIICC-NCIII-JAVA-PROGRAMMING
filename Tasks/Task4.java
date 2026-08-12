82780c2910b0fe05edb46ed7baf7ffc194e24eaa
Allan Angelo Del Mundo <delmundo.allanangelo.pili@gmail.com>
2026-08-12 06:16:45 +0000

    Task4

---
+++ Tasks/Task4.java
@@ -0,0 +1,23 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        
+        StringBuilder sb = new StringBuilder(input);
+        String reversed = sb.reverse().toString(); // Convert it back to a regular String
+        
+        if (input.equals(reversed)) {
+            System.out.println("The input string is a palindrome.");
+        } else {
+            System.out.println("The input string is not a palindrome.");
+        }
+        
+        scanner.close();
+    }
+}
