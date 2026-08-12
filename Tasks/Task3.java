19987c8c9b77dd8848fd9a96a7e4779267bc5ec3
Allan Angelo Del Mundo <delmundo.allanangelo.pili@gmail.com>
2026-08-12 06:11:04 +0000

    Task 3

---
+++ Tasks/Task3.java
@@ -0,0 +1,23 @@
+package Tasks;
+
+public class Task3 {
+    public static void main(String[] args) {
+ 
+        String a = "Wow";
+        String b = a;            
+        String c = "Different";  
+        String d = "Wow!";      
+
+    
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+  
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        } else {
+            System.out.println("Failed! Check your variable assignments.");
+        }
+    }
+}
